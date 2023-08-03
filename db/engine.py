b5ebbf19ac87f11542a32657c7b76177da7309e7
Nikita Provorov <nikiyorkovich@gmail.com>
2023-08-03 16:24:14 +0300

    Implement database

---
+++ db/engine.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = URL.create(
+    drivername="postgresql",
+    username="postgres",
+    host="localhost",
+    database="postgres",
+    password="postgres",
+)
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+print(SQLALCHEMY_DATABASE_URL)
---
+++ db/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+
+from db.engine import Base
+
+
+class DBContacts(Base):
+    __tablename__ = "contact"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(255))
+    last_name = Column(String(255))
+    email = Column(String(255))
+
