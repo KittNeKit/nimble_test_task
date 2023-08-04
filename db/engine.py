3114d80bdb9826f2e120c7951c5ec152b386eb97
Nikita Provorov <nikiyorkovich@gmail.com>
2023-08-04 13:52:01 +0300

    Create .env_sample

--- db/engine.py
+++ db/engine.py
@@ -1,14 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = URL.create(
     drivername="postgresql",
-    username="postgres",
-    host="localhost",
-    database="postgres",
-    password="postgres",
+    username=os.getenv("POSTGRES_USERNAME"),
+    host=os.getenv("POSTGRES_HOST"),
+    database=os.getenv("POSTGRES_DATABASE"),
+    password=os.getenv("POSTGRES_PASSWORD"),
 )
 
 engine = create_engine(
